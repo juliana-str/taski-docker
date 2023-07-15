b1521d5a4a3d6fd13bbb1b56ebe3123d152bc5fe
Юлиана Стрельникова <julianka.str@gmail.com>
2023-07-15 19:43:40 +0300

    once more time

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,8 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', '1234')
 
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
 ALLOWED_HOSTS = ['158.160.30.4', '127.0.0.1', 'localhost', 'taskieducation.hopto.org']
 
 # Application definition
